37098786f9b2499277bfa2b396b0eb72a07aa273
panfilov-oleg <36545228+panfilov-oleg@users.noreply.github.com>
2019-11-26 10:05:27 +0300

    Create org.user.js

---
+++ org.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         OpenHide_Org
+// @namespace    openhide
+// @version      0.1
+// @description  try to take over the world!
+// @author       Rbex
+// @include      https://open-hide.biz/*
+// @match        https://open-hide.biz/*
+// @grant        none
+// @require      users.js?2
+// @require      participantColor.js?2
+// ==/UserScript==
