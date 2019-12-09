c2b5eb190a1b1a75e5f46ebe790b2e32b73e39da
panfilov-oleg <36545228+panfilov-oleg@users.noreply.github.com>
2019-12-09 12:24:30 +0300

    Update org.user.js

--- org.user.js
+++ org.user.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name         OpenHide_Org
 // @namespace    openhide
-// @version      0.1
-// @description  try to take over the world!
-// @author       Rbex
-// @include      https://open-hide.biz/*
-// @match        https://open-hide.biz/*
-// @grant        none
-// @require      users.js?2
-// @require      participantColor.js?2
+// @version      0.4
+// @grant        GM_addStyle
+// @description  Additional features for OpenHide.biz
+// @author       Rbex, Veenom, IRobot
+// @include      https://openhide.biz/*
+// @match        https://openhide.biz/*
+// @require      users.js?4
+// @require      participantColor.js?4
+// @require      calculator.js?4
 // ==/UserScript==
